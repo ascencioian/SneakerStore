0999c623dd27af322177461af13b086b3e0181a9
Ian Ascencio <ian.ascencio.mancia@gmail.com>
2022-06-01 14:31:56 -0400

    implemented form control for register modal

--- js/index.js
+++ js/index.js
@@ -6,71 +6,68 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const passwordCheck = document.getElementById('passwordCheck');
 
-
-form.addEventListener('submit', (e) => {
+form.addEventListener('click', (e) => {
 	e.preventDefault();
-    console.log("hello")
+   
 	checkInputs();
 });
 
 function checkInputs() {
 	// trim to remove the whitespaces
-    console.log("hello")
 	const firstNameValue = firstName.value.trim();
     const lastNameValue = LastName.value.trim();
 	const emailValue = email.value.trim();
 	const passwordValue = password.value.trim();
 	const passwordCheckValue = passwordCheck.value.trim();
 	
-	// if(firstNameValue === '') {
-	// 	setErrorFor(firstName, 'First name cannot be blank');
-	// } else {
-	// 	setSuccessFor(firstName);
-	// }
+	if(firstNameValue === '') {
+		setErrorFor(firstName, 'First name cannot be blank');
+	} else {
+		setSuccessFor(firstName);
+	}
 
     if(lastNameValue === '') {
+        console.log("hello")
 		setErrorFor(LastName, 'Last name cannot be blank');
         console.log('Last name cannot be blank')
 	} else {
 		setSuccessFor(LastName);
 	}
 	
-	// if(emailValue === '') {
-	// 	setErrorFor(email, 'Email cannot be blank');
-	// } else if (!isEmail(emailValue)) {
-	// 	setErrorFor(email, 'Not a valid email');
-	// } else {
-	// 	setSuccessFor(email);
-	// }
+	if(emailValue === '') {
+		setErrorFor(email, 'Email cannot be blank');
+	} else if (!isEmail(emailValue)) {
+		setErrorFor(email, 'Not a valid email');
+	} else {
+		setSuccessFor(email);
+	}
 	
-	// if(passwordValue === '') {
-	// 	setErrorFor(password, 'Password cannot be blank');
-	// } else {
-	// 	setSuccessFor(password);
-	// }
+	if(passwordValue === '') {
+		setErrorFor(password, 'Password cannot be blank');
+	} else {
+		setSuccessFor(password);
+	}
 	
-	// if(password2Value === '') {
-	// 	setErrorFor(password2, 'Password2 cannot be blank');
-	// } else if(passwordValue !== password2Value) {
-	// 	setErrorFor(password2, 'Passwords does not match');
-	// } else{
-	// 	setSuccessFor(password2);
-	// }
+	if(passwordCheckValue === '') {
+		setErrorFor(passwordCheck, 'Password confirmation cannot be blank');
+	} else if(passwordValue !== passwordCheckValue) {
+		setErrorFor(passwordCheck, 'Passwords does not match');
+	} else{
+		setSuccessFor(passwordCheck);
+	}
 }
 
 function setErrorFor(input, message) {
-	const formControl = input;
+	const formControl = input.parentElement;
 	const small = formControl.querySelector('small');
-    console.log(small)
-	formControl.className = 'form-control formControlSuccess';
+	input.className = 'form-control formControlError';
 	small.innerText = message;
 }
 
-// function setSuccessFor(input) {
-// 	const formControl = input.parentElement;
-// 	formControl.className = 'form-control success';
-// }
+function setSuccessFor(input) {
+	input.className = 'form-control formControlSuccess';
+}
 	
-// function isEmail(email) {
-// 	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
-// }
+function isEmail(email) {
+	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
